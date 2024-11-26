cb0d920fcb3f72acbf00a5d92c179ee3691fd687
Stuart MacKay <smackay@flagstonesoftware.com>
2024-11-26 19:34:15 +0000

    refactor: Remove unnecessary, default views

--- backend/main/views/__init__.py
+++
@@ -1,9 +0,0 @@
-from .index import IndexView
-from .robots import RobotsView
-from .sitemap import SitemapView
-
-__all__ = (
-    "IndexView",
-    "RobotsView",
-    "SitemapView",
-)
--- backend/main/views/robots.py
+++
@@ -1,6 +0,0 @@
-from django.views import generic
-
-
-class RobotsView(generic.TemplateView):
-    template_name = "robots.txt"
-    content_type = "text/plain"
